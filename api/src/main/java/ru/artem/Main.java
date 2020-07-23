82b3ec813688aeb062e7655f76d9fdb51e7a860f
Artem Prytkov <aprytkov@gmail.com>
2020-07-23 14:01:32 +0300

    Initial commit

---
+++ api/src/main/java/ru/artem/Main.java
@@ -0,0 +1,15 @@
+package ru.artem;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main(String[] args) {
+        MyService myService = new MyService();
+        System.out.println(myService.getMyEntity());
+        System.out.println(myService.setMyEntity(new MyEntity("second")));
+        System.out.println(myService.getMyEntity());
+    }
+}
